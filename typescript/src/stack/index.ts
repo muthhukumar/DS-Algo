23f75be03d6b29bcbc010dec16d33482fbed0883
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-06-26 18:44:19 +0530

    feat(stack): implemented stack using linked list

---
+++ typescript/src/stack/index.ts
@@ -0,0 +1,50 @@
+class SNode<T> {
+  value: T;
+  prev?: SNode<T>;
+  constructor(value: T, prev?: SNode<T>) {
+    this.value = value;
+    this.prev = prev;
+  }
+}
+
+class Stack<T> {
+  length: number;
+  head?: SNode<T>;
+
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
+
+  push(value: T): void {
+    const node = new SNode(value);
+
+    this.length++;
+
+    if (!this.head) {
+      this.head = node;
+
+      return;
+    }
+
+    node.prev = this.head;
+
+    this.head = node;
+  }
+
+  pop(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+
+    const head = this.head;
+
+    this.head = this.head.prev;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
