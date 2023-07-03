883223ee2bbaebadecb66658b42aeddf87151729
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-03 23:01:33 +0530

    fix

---
+++ typescript/src/DS/index.ts
@@ -0,0 +1,3 @@
+export { default as Queue } from "./Queue";
+export { default as Stack } from "./Stack";
+export { default as DoublyLinkedList } from "./DoublyLinkedList";
--- typescript/src/ds/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Queue } from "./queue";
-export { default as Stack } from "./stack";
-export { default as DoublyLinkedList } from "./DoublyLinkedList";
