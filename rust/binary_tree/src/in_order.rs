d6b9df0975f2e9b91640bdbe0c65f176534530d1
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-05 23:14:41 +0530

    cleanup

--- rust/binary_tree/src/in_order.rs
+++ rust/binary_tree/src/in_order.rs
@@ -78,52 +78,6 @@ mod tests {
             }))),
         };
 
-        // let tree2 = BinaryNode {
-        //     value: 20,
-        //     right: Some(Rc::new(RefCell::new(BinaryNode {
-        //         value: 50,
-        //         right: None,
-        //         left: Some(Rc::new(RefCell::new(BinaryNode {
-        //             value: 30,
-        //             right: Some(Rc::new(RefCell::new(BinaryNode {
-        //                 value: 45,
-        //                 left: None,
-        //                 right: Some(Rc::new(RefCell::new(BinaryNode {
-        //                     value: 49,
-        //                     left: None,
-        //                     right: None,
-        //                 }))),
-        //             }))),
-        //             left: Some(Rc::new(RefCell::new(BinaryNode {
-        //                 value: 29,
-        //                 right: None,
-        //                 left: Some(Rc::new(RefCell::new(BinaryNode {
-        //                     value: 21,
-        //                     left: None,
-        //                     right: None,
-        //                 }))),
-        //             }))),
-        //         }))),
-        //     }))),
-        //     left: Some(Rc::new(RefCell::new(BinaryNode {
-        //         value: 10,
-        //         right: Some(Rc::new(RefCell::new(BinaryNode {
-        //             value: 15,
-        //             left: None,
-        //             right: None,
-        //         }))),
-        //         left: Some(Rc::new(RefCell::new(BinaryNode {
-        //             value: 5,
-        //             left: None,
-        //             right: Some(Rc::new(RefCell::new(BinaryNode {
-        //                 value: 7,
-        //                 left: None,
-        //                 right: None,
-        //             }))),
-        //         }))),
-        //     }))),
-        // };
-
         assert_eq!(
             in_order_search(tree),
             vec![5, 7, 10, 15, 20, 29, 30, 45, 50, 100]
