5a905797b56964b7421abf6c847d172a00e06a43
muthukumar <muthukumarperiyasamy.mk@gmail.com>
2023-07-08 13:19:20 +0530

    feat(doubly-linked-list): finally implemented doubly linked list in rust
    fix #19

--- rust/ds/src/doubly_linked_list.rs
+++ rust/ds/src/doubly_linked_list.rs
@@ -1,25 +1,29 @@
-use std::{cell::RefCell, fmt::Debug as TDebug, rc::Rc};
+use std::{
+    cell::RefCell,
+    fmt::Debug as TDebug,
+    rc::{Rc, Weak},
+};
 
 #[derive(Debug)]
 struct DLNode<T: Clone + PartialEq + TDebug> {
     value: T,
-    prev: Option<Rc<RefCell<DLNode<T>>>>,
+    prev: Option<Weak<RefCell<DLNode<T>>>>,
     next: Option<Rc<RefCell<DLNode<T>>>>,
 }
 
 impl<T: Clone + PartialEq + TDebug> DLNode<T> {
-    pub fn new(value: T) -> DLNode<T> {
-        DLNode {
+    pub fn new(value: T) -> Rc<RefCell<DLNode<T>>> {
+        Rc::new(RefCell::new(DLNode {
             value,
             prev: None,
             next: None,
-        }
+        }))
     }
 }
 
 pub struct DoublyLinkedList<T: Clone + PartialEq + TDebug> {
     head: Option<Rc<RefCell<DLNode<T>>>>,
-    tail: Option<Rc<RefCell<DLNode<T>>>>,
+    tail: Option<Weak<RefCell<DLNode<T>>>>,
     length: usize,
 }
 
@@ -34,193 +38,170 @@ impl<T: Clone + PartialEq + TDebug> DoublyLinkedList<T> {
 
     pub fn append(&mut self, value: T) {
         self.length += 1;
-        let node = Rc::new(RefCell::new(DLNode::new(value)));
+        let node = DLNode::new(value);
 
         match self.tail.take() {
             Some(tail) => {
-                node.borrow_mut().prev = Some(Rc::clone(&tail));
-                tail.borrow_mut().next = Some(Rc::clone(&node));
+                if let Some(tail) = tail.upgrade() {
+                    node.borrow_mut().prev = Some(Rc::downgrade(&tail));
+                    tail.borrow_mut().next = Some(Rc::clone(&node));
+                } else {
+                    self.head = Some(Rc::clone(&node));
+                }
 
-                self.tail = Some(Rc::clone(&node));
+                self.tail = Some(Rc::downgrade(&node));
             }
             None => {
                 self.head = Some(Rc::clone(&node));
-                self.tail = Some(Rc::clone(&node));
+                self.tail = Some(Rc::downgrade(&node));
             }
         }
     }
     pub fn prepend(&mut self, value: T) {
         self.length += 1;
 
-        let node = Rc::new(RefCell::new(DLNode::new(value)));
+        let node = DLNode::new(value);
 
         match self.head.take() {
             Some(head) => {
                 node.borrow_mut().next = Some(Rc::clone(&head));
-                head.borrow_mut().prev = Some(Rc::clone(&node));
+                head.borrow_mut().prev = Some(Rc::downgrade(&node));
 
                 self.head = Some(Rc::clone(&node));
             }
             None => {
                 self.head = Some(Rc::clone(&node));
-                self.tail = Some(Rc::clone(&node));
+                self.tail = Some(Rc::downgrade(&node));
             }
         }
     }
-    pub fn insert_at(&mut self, item: T, idx: usize) {
+    pub fn insert_at(&mut self, idx: usize, item: T) {
         if idx == 0 {
             return self.prepend(item);
-        } else if idx == self.length {
+        } else if idx == self.length() {
             return self.append(item);
         }
 
-        match self.get_at(idx) {
-            Some(curr) => {
-                self.length += 1;
+        if let Some(curr) = self.get_at(idx) {
+            self.length += 1;
 
-                let node = Rc::new(RefCell::new(DLNode::new(item)));
+            let node = DLNode::new(item);
 
-                node.borrow_mut().next = Some(Rc::clone(&curr));
+            node.borrow_mut().next = Some(Rc::clone(&curr));
 
-                if let Some(curr_prev) = curr.borrow().prev.clone() {
-                    node.borrow_mut().prev = Some(Rc::clone(&curr_prev));
-                    curr_prev.borrow_mut().next = Some(Rc::clone(&node));
-                }
-
-                curr.borrow_mut().prev = Some(Rc::clone(&node));
-            }
-            None => {}
+            curr.borrow()
+                .prev
+                .as_ref()
+                .and_then(|prev| prev.upgrade())
+                .map(|prev| {
+                    node.borrow_mut().prev = Some(Rc::downgrade(&prev));
+                    curr.borrow_mut().prev = Some(Rc::downgrade(&node));
+                    prev.borrow_mut().next = Some(Rc::clone(&node));
+                });
         }
     }
     pub fn remove(&mut self, value: T) -> Option<T> {
-        let curr = match &self.head {
-            Some(head) => {
-                let mut curr = head.clone();
+        let mut idx = 0;
 
-                for _ in 0..self.length {
-                    if curr.borrow().value == value {
-                        break;
-                    }
-                    if let Some(next) = curr.clone().borrow().next.clone() {
-                        curr = next;
-                    }
-                }
+        let curr = {
+            let mut curr = self.head.clone();
 
-                Some(curr)
+            while let Some(node) = curr {
+                if value == node.borrow().value {
+                    curr = Some(node);
+                    break;
+                };
+
+                idx += 1;
+                curr = node.borrow().next.clone();
             }
-            None => return None,
+
+            curr
         };
 
-        match curr {
-            Some(curr) => {
-                self.length -= 1;
-
-                if let Some(head) = &self.head {
-                    if Rc::ptr_eq(head, &curr) {
-                        if let Some(head) = self.head.take() {
-                            self.head = head.borrow().next.clone();
-
-                            head.borrow_mut().next = None;
-                            head.borrow_mut().prev = None;
-
-                            return Some(head.borrow().value.clone());
-                        }
-                    }
-                }
-                if let Some(tail) = &self.tail {
-                    if Rc::ptr_eq(tail, &curr) {
-                        if let Some(tail) = self.tail.take() {
-                            self.tail = tail.borrow().prev.clone();
-
-                            tail.borrow_mut().next = None;
-                            tail.borrow_mut().prev = None;
-
-                            return Some(tail.borrow().value.clone());
-                        }
-                    }
-                }
-
-                let prev = curr.borrow().prev.clone();
-                let next = curr.borrow().next.clone();
-
-                match (&prev, &next) {
-                    (Some(curr_prev), Some(curr_next)) => {
-                        curr_prev.borrow_mut().next = Some(Rc::clone(&curr_next));
-                    }
-                    _ => {}
-                }
-
-                match (&prev, &next) {
-                    (Some(curr_prev), Some(curr_next)) => {
-                        curr_next.borrow_mut().prev = Some(Rc::clone(&curr_prev));
-                    }
-                    _ => {}
-                }
-
-                Some(curr.borrow().value.clone())
-            }
-            None => None,
+        if idx > self.length {
+            return None;
         }
+
+        self.length -= 1;
+
+        if idx == 0 {
+            let head = self.head.clone();
+            let value = head.as_ref().unwrap().borrow().value.clone();
+
+            if let Some(next) = head.unwrap().borrow().next.clone() {
+                self.head = Some(next);
+            }
+
+            return Some(value);
+        } else if idx == self.length() {
+            let tail = self.tail.clone().as_ref().unwrap().upgrade().unwrap();
+            let value = tail.borrow().value.clone();
+
+            tail.borrow().prev.as_ref().unwrap().upgrade().map(|prev| {
+                self.tail = Some(Rc::downgrade(&prev));
+            });
+
+            return Some(value);
+        }
+
+        let curr_prev = curr.as_ref().unwrap().borrow().prev.clone();
+        let curr_next = curr.as_ref().unwrap().borrow().next.clone();
+
+        curr_prev.and_then(|prev| prev.upgrade()).map(|prev| {
+            prev.borrow_mut().next = Some(curr_next.clone().unwrap().clone());
+        });
+
+        curr_next.and_then(|next| Some(next)).map(|next| {
+            next.borrow_mut().prev = Some(Rc::downgrade(&next));
+        });
+
+        let value = curr.as_ref().unwrap().borrow().value.clone();
+
+        Some(value)
     }
     pub fn remove_at(&mut self, idx: usize) -> Option<T> {
-        match self.get_at(idx) {
-            Some(curr) => {
-                self.length -= 1;
+        if idx > self.length {
+            return None;
+        }
 
-                if let Some(head) = &self.head {
-                    if Rc::ptr_eq(head, &curr) {
-                        if let Some(head) = self.head.take() {
-                            self.head = head.borrow().next.clone();
+        self.length -= 1;
 
-                            head.borrow_mut().next = None;
-                            head.borrow_mut().prev = None;
+        if idx == 0 {
+            let head = self.head.clone();
+            let value = head.as_ref().unwrap().borrow().value.clone();
 
-                            return Some(head.borrow().value.clone());
-                        }
-                    }
-                }
-                if let Some(tail) = &self.tail {
-                    // println!("tail: {:?}", tail);
-                    // println!("curr: {:?}", curr);
-                    if Rc::ptr_eq(tail, &curr) {
-                        println!("it should come to this part");
-                        if let Some(tail) = self.tail.take() {
-                            self.tail = tail.borrow().prev.clone();
-
-                            tail.borrow_mut().next = None;
-                            tail.borrow_mut().prev = None;
-
-                            return Some(tail.borrow().value.clone());
-                        }
-                    }
-                }
-
-                let prev = curr.borrow().prev.clone();
-                let next = curr.borrow().next.clone();
-
-                match (&prev, &next) {
-                    (Some(curr_prev), Some(curr_next)) => {
-                        curr_prev.borrow_mut().next = Some(Rc::clone(&curr_next));
-                    }
-                    _ => {}
-                }
-
-                match (&prev, &next) {
-                    (Some(curr_prev), Some(curr_next)) => {
-                        curr_next.borrow_mut().prev = Some(Rc::clone(&curr_prev));
-                    }
-                    _ => {}
-                }
-
-                Some(curr.borrow().value.clone())
+            if let Some(next) = head.unwrap().borrow().next.clone() {
+                self.head = Some(next);
             }
-            None => {
-                println!("It came to his None");
-                return None;
-            }
-        };
 
-        None
+            return Some(value);
+        } else if idx == self.length() {
+            let tail = self.tail.clone().as_ref().unwrap().upgrade().unwrap();
+            let value = tail.borrow().value.clone();
+
+            tail.borrow().prev.as_ref().unwrap().upgrade().map(|prev| {
+                self.tail = Some(Rc::downgrade(&prev));
+            });
+
+            return Some(value);
+        }
+        let curr = self.get_at(idx);
+
+        let curr_prev = curr.as_ref().unwrap().borrow().prev.clone();
+        let curr_next = curr.as_ref().unwrap().borrow().next.clone();
+
+        curr_prev.and_then(|prev| prev.upgrade()).map(|prev| {
+            prev.borrow_mut().next = Some(curr_next.clone().unwrap().clone());
+        });
+
+        curr_next.and_then(|next| Some(next)).map(|next| {
+            next.borrow_mut().prev = Some(Rc::downgrade(&next));
+        });
+
+        let value = curr.as_ref().unwrap().borrow().value.clone();
+
+        Some(value)
     }
     pub fn get(&self, idx: usize) -> Option<T> {
         match self.get_at(idx) {
@@ -233,20 +214,19 @@ impl<T: Clone + PartialEq + TDebug> DoublyLinkedList<T> {
     }
 
     fn get_at(&self, idx: usize) -> Option<Rc<RefCell<DLNode<T>>>> {
-        match &self.head {
-            Some(head) => {
-                let mut curr = head.clone();
+        let mut current = self.head.clone();
+        let mut curr_idx = 0;
 
-                for _ in 0..idx {
-                    if let Some(next) = curr.clone().borrow().next.clone() {
-                        curr = next;
-                    }
-                }
-
-                Some(curr)
+        while let Some(node) = current {
+            if curr_idx == idx {
+                return Some(node);
             }
-            None => None,
+
+            current = node.borrow().next.clone();
+            curr_idx += 1;
         }
+
+        None
     }
 }
 
@@ -263,27 +243,64 @@ mod tests {
         list.append(7);
         list.append(9);
 
-        println!("size: {}", list.length);
-        assert_eq!(list.get(2).unwrap(), 9);
-        println!("get at 2: {}", list.get(2).unwrap());
-        assert_eq!(list.remove_at(1).unwrap(), 7);
-        assert_eq!(list.length, 2);
+        list.prepend(19);
 
-        list.append(11);
-        assert_eq!(list.remove_at(1).unwrap(), 9);
+        // list: 19, 5, 7, 9
+
+        assert_eq!(list.get(0).unwrap(), 19);
+        assert_eq!(list.length(), 4);
+
+        list.insert_at(0, 100);
+
+        // list: 100, 19, 5, 7, 9
+
+        assert_eq!(list.remove(100).unwrap(), 100);
+
+        // list: 19, 5, 7, 9
+
+        assert_eq!(list.get(0).unwrap(), 19);
         assert_eq!(list.remove(9).unwrap(), 9);
+
+        // list: 19, 5, 7
+
+        assert_eq!(list.length(), 3);
+        assert_eq!(list.get(2).unwrap(), 7);
+        assert_eq!(list.get(0).unwrap(), 19);
+        assert_eq!(list.get(1).unwrap(), 5);
+
+        list.prepend(84);
+        list.prepend(34);
+
+        // list: 34, 84, 19, 5, 7
+        assert_eq!(list.length(), 5);
+        assert_eq!(list.get(0).unwrap(), 34);
+        assert_eq!(list.get(1).unwrap(), 84);
+        assert_eq!(list.get(2).unwrap(), 19);
+        assert_eq!(list.get(3).unwrap(), 5);
+        assert_eq!(list.get(4).unwrap(), 7);
+
+        // list: 34, 84, 19, 5, 7
+        assert_eq!(list.remove(19).unwrap(), 19);
+        assert_eq!(list.length(), 4);
+        assert_eq!(list.get(3).unwrap(), 7);
+
+        // list: 34, 84, 5, 7
+
+        assert_eq!(list.get(0).unwrap(), 34);
+        assert_eq!(list.remove_at(7), None);
+
+        // list: 34, 84, 5, 7
+        //
+        assert_eq!(list.remove_at(1).unwrap(), 84);
+
+        // list: 34, 5, 7
+
+        assert_eq!(list.length, 3);
+        assert_eq!(list.remove_at(0).unwrap(), 34);
+        assert_eq!(list.remove_at(1).unwrap(), 7);
         assert_eq!(list.remove_at(0).unwrap(), 5);
-        assert_eq!(list.remove_at(0).unwrap(), 11);
         assert_eq!(list.length, 0);
 
-        list.prepend(5);
-        list.prepend(7);
-        list.prepend(9);
-
-        assert_eq!(list.get(2).unwrap(), 5);
-        assert_eq!(list.get(0).unwrap(), 9);
-        assert_eq!(list.remove(9).unwrap(), 9);
-        assert_eq!(list.length, 2);
-        assert_eq!(list.get(0).unwrap(), 7);
+        // list: empty
     }
 }
