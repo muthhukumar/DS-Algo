d44ac570edac438a795b24ffe93e5a6da1ff44dd
muthhukumar <muthukumarperiyasamy.mk@gmail.com>
2024-08-13 21:28:47 +0530

    updated the solution for longest consecutive sequence

--- leetcode/0128-longest-consecutive-sequence.ts
+++ leetcode/0128-longest-consecutive-sequence.ts
@@ -1,4 +1,5 @@
 /**
+ * After Watching the solution
  * @param {number[]} nums
  * @return {number}
  */
@@ -17,6 +18,55 @@ var longestConsecutive = function (nums) {
 
       result = Math.max(result, count);
     }
+  }
+
+  return result;
+};
+
+/**
+ * Initial solution without watching solution
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function (nums) {
+  const hashmap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] in hashmap)) {
+      hashmap[nums[i]] = i;
+    }
+  }
+
+  // I later realized this is unnecessary. Tried to be smart but didn't worked out. This is a shortcut to the solution.
+  for (let i = 0; i < nums.length; i++) {
+    if (!(nums[i] - 1 in hashmap) && !(nums[i] + 1 in hashmap)) {
+      delete hashmap[nums[i]];
+    }
+  }
+
+  let result = 0;
+
+  let min = null;
+  let max = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    let start = nums[i];
+
+    if (min && max && start > min && start < max) continue;
+
+    let count = 1;
+
+    while (start + 1 in hashmap) {
+      start = start + 1;
+      count += 1;
+    }
+
+    if (count > result) {
+      min = nums[i];
+      max = start;
+    }
+
+    result = Math.max(result, count);
   }
 
   return result;
